a938bdaca8dd09aea8d64b1e10ecc20f6d8b7e9e
MinesVanKempen <43882808+MinesVanKempen@users.noreply.github.com>
2020-01-03 18:38:22 +0100

    chargement de la structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" >
+        <title>Projet BDD</title>
+		
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/css/style.css">		
+        
+    </head>
+    <body>            
+        
+		
+    </body>
+</html>

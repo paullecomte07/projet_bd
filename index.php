cf0a046b93811eaba6c97841af7a323a04673155
Paul Lecomte <paullecomte07@gmail.com>
2020-01-04 20:22:07 +0100

    add a dropdown list and add button for csv file

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
       <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
       <link rel="stylesheet" href="bootstrap/css/style.css">
       <link rel="stylesheet" href="css_stylesheet/index.css">
+      <script src="bootstrap/js/jquery.js"></script>
+      <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="bootstrap/js/bootstrap.bundle.js"></script>
 
   </head>
   <body>
@@ -27,17 +30,66 @@
             echo "This is Button1 that is selected";
         }
         function button2() {
-            echo "This is Button2 that is selected";
+          $csvFile = file('Enregistrements/Patient.csv');
+          $data = [];
+          foreach ($csvFile as $line) {
+            echo($line);
+            echo("coucou");
+            $data[] = str_getcsv($line);
+          }
+          echo($data[2]);
         }
     ?>
+
   <div class="conteneur-button">
     <form method="post">
         <input type="submit" name="button1" class="btn btn-primary m-2" value="Créer une nouvelle base" />
-
-        <input type="submit" name="button2" class="btn btn-secondary m-2" value="Ajouter des éléments" />
         <input type="submit" name="button2" class="btn btn-danger m-2" value="Effacer la base de donnée" />
     </form>
   </div>
+
+
+  <div class="conteneur-button">
+      <div class="row">
+          <form class="form-horizontal" action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
+              <fieldset>
+
+                  <div class="form-group">
+                      <label class="col-md-4 control-label" for="filebutton">Ajouter des fichiers</label>
+                      <div class="col-md-4">
+                          <input type="file" name="file" id="file" class="input-large btn btn-primary">
+                      </div>
+                  </div>
+                  <!-- Button -->
+                  <div class="form-group">
+
+                      <div class="col-md-4">
+                          <button type="submit" id="submit" name="Import" class="btn btn-success button-loading" data-loading-text="Loading...">Importer</button>
+                      </div>
+                  </div>
+              </fieldset>
+          </form>
+      </div>
+
+  </div>
+
+
+  <div class="dropdown show">
+    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown link
+    </a>
+
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+
+    </div>
+  </div>
+
+  <div>
+    <div class="requete"> s</div>
+  </div>
+
 
   </body>
 </html>

fcf32d146b271cc7d39c717d97d518cc5070814d
MinesVanKempen <43882808+MinesVanKempen@users.noreply.github.com>
2020-01-07 21:29:55 +0100

    update

--- fonctions.php
+++ fonctions.php
@@ -18,44 +18,56 @@
 
   function creerTables () 
 {
-  $instructionSQL1="CREATE TABLE Service(NumService INT(1) NOT NULL, Nom VARCHAR(50) NOT NULL, Batiment VARCHAR(1) NOT NULL, NumMed INT(2) NOT NULL);";
+  $instructionSQL1="CREATE TABLE Service(
+    NumService INT(1) NOT NULL,
+    Nom VARCHAR(50) NOT NULL,
+    Batiment VARCHAR(1) NOT NULL,
+    NumMed INT(2) NOT NULL,
+    PRIMARY KEY (NumService)
+  );";
 
   $instructionSQL2='CREATE TABLE Salle(
     NumSalle INT(5) NOT NULL,
     NumService INT(5) NOT NULL,
     Nblits Int(5) NOT NULL,
-    NumInf INT(5) NOT NULL
+    NumInf INT(5) NOT NULL,
+    PRIMARY KEY (numSalle)
   );';
   $instructionSQL3='CREATE TABLE Infirmier (
     NumInf INT(5) NOT NULL,
     Nom VARCHAR(50) NOT NULL,
     Adresse VARCHAR(50) NOT NULL,
-    Telphone INT(10) NOT NULL
+    Telphone INT(10) NOT NULL,
+    PRIMARY KEY (NumInf)
   );';
   $instructionSQL4='CREATE TABLE Patient(
     NumPat INT(5) NOT NULL,
     Nom VARCHAR(50) NOT NULL,
     Prenom VARCHAR(50) NOT NULL,
-    Mutuelle VARCHAR(50) NOT NULL
+    Mutuelle VARCHAR(50) NOT NULL,
+    PRIMARY KEY (NumPat)
   );';
   $instructionSQL5='CREATE TABLE Medecin(
     NumMed INT(5) NOT NULL,
     nom VARCHAR(50) NOT NULL,
-    Specialite VARCHAR(50) NOT NULL
+    Specialite VARCHAR(50) NOT NULL,
+    PRIMARY KEY (NumMed)
   );';
   $instructionSQL6='CREATE TABLE Hospitalisation(
     NumPat INT(5) NOT NULL,
     DateEntree VARCHAR(10) NOT NULL,
     NumSalle INT(5) NOT NULL,
     NumService INT(5) NOT NULL,
-    DateSortie VARCHAR(10) NOT NULL
+    DateSortie VARCHAR(10) NOT NULL,
+    PRIMARY KEY (NumPat, DateEntree, NumSalle)
   );';
   $instructionSQL7='CREATE TABLE Acte(
     NumMed INT(5) NOT NULL,
     NumPat INT(5) NOT NULL,
     DateAct VARCHAR(10) NOT NULL,
     NumService INT(5) NOT NULL,
-    Description VARCHAR(50) NOT NULL
+    Description VARCHAR(50) NOT NULL,
+    PRIMARY KEY (NumMed, NumPat, DateAct)
   );';
 
   for ($i = 1; $i <= 7; $i++) {
@@ -65,7 +77,7 @@
     $sql= ${'instructionSQL'.$i};
     //echo($sql);
     $req = mysqli_query($base,$sql)
-      or die("Impossible de créer la table : ");
+      or die("Impossible de créer la table : ".$i);
     
   }
   return TRUE;
